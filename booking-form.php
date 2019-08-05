cbe4e2e71851417fc68d833c56716e617a18cedf
ADinushika <amalidinushika93@gmail.com>
2019-08-05 11:46:14 +0530

    #21 Corrections of view tour package page

--- booking-form.php
+++ booking-form.php
@@ -1,19 +1,17 @@
-<div id="booking-form" class="tab-content">
-    <div class="">
-        <div class="contactus-form"  id="contact">
-            <div id="message"></div>
-            <form method="post" action="https://grandetest.com/theme/prolist-html/contact.php" name="contactform" id="contactform">
-                <div class="row">
-                    <div class="col-md-6"><input name="name" type="text" id="name" placeholder="Name" /></div>
-                    <div class="col-md-6"><input  name="email" type="text" id="email" placeholder="Email"  /></div>
-                    <div class="col-md-6"><input type="text" placeholder="No of Adults" /></div>
-                    <div class="col-md-6"><input type="text" placeholder="No of Children" /></div>
-                    <div class="col-md-6"><input type="text" placeholder="Tour Package" /></div>
-                    <div class="col-md-6"><input type="text" placeholder="Subject" /></div>
-                    <div class="col-md-12"><textarea name="comments" id="comments" placeholder="Message"></textarea></div>
-                    <div class="col-md-12"><input type="submit" id="submit" value="Book Now" /></div>
-                </div>
-            </form>
-        </div>
+<div class="">
+    <div class="contactus-form"  id="contact">
+        <div id="message"></div>
+        <form method="post" action="https://grandetest.com/theme/prolist-html/contact.php" name="contactform" id="contactform">
+            <div class="row">
+                <div class="col-md-6"><input name="name" type="text" id="name" placeholder="Name" /></div>
+                <div class="col-md-6"><input  name="email" type="text" id="email" placeholder="Email"  /></div>
+                <div class="col-md-6"><input type="text" placeholder="No of Adults" /></div>
+                <div class="col-md-6"><input type="text" placeholder="No of Children" /></div>
+                <div class="col-md-6"><input type="text" placeholder="Tour Package" /></div>
+                <div class="col-md-6"><input type="text" placeholder="Subject" /></div>
+                <div class="col-md-12"><textarea name="comments" id="comments" placeholder="Message"></textarea></div>
+                <div class="col-md-12"><input type="submit" id="submit" value="Book Now" /></div>
+            </div>
+        </form>
     </div>
 </div>
